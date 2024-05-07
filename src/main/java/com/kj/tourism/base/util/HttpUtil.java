c97343cb3a2455d543bc7475a0c30fe0f39739ad
seouljun <kkj5270@naver.com>
2024-05-07 22:21:43 +0900

    URI 만드는 Util 수정

--- src/main/java/com/kj/tourism/base/util/HttpUtil.java
+++ src/main/java/com/kj/tourism/base/util/HttpUtil.java
@@ -1,11 +1,13 @@
 package com.kj.tourism.base.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kj.tourism.business.tourismConst.TourismConst;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.net.URI;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,44 +15,50 @@ public class HttpUtil {
     private static final String QUESTION_MARK = "?";
     private static final String AMPERSAND = "&";
     private static final String EQUAL = "=";
+    private static final String SLASH = "/";
     private static final String ERROR_MESSAGE = "parameter map is null";
 
     /**
      * MAP TO REQUEST PARAMETER
      */
-    public static String makeRequestParam(Map<String, String> map) {
+    public static URI makeRequestParam(Map<String, ?> map, String method) {
         StringBuilder sb;
-
+        URI uri = null;
         if (ObjectUtils.isNotEmpty(map)) {
-            Set<Map.Entry<String, String>> entrySet = map.entrySet();
+            Set<? extends Map.Entry<String, ?>> entrySet = map.entrySet();
 
             sb = new StringBuilder();
+            sb.append(TourismConst.URL);
+            sb.append(method);
             sb.append(QUESTION_MARK);
 
-            for (Map.Entry<String, String> entry : entrySet) {
+            for (Map.Entry<String, ?> entry : entrySet) {
                 String key = entry.getKey();
-                String value = entry.getValue();
-                sb.append(key).append(EQUAL).append(value).append(AMPERSAND);
+                Object value = entry.getValue();
+                if(ObjectUtils.isNotEmpty(value))
+                {
+                    sb.append(key).append(EQUAL).append(value.toString()).append(AMPERSAND);
+                }
             }
 
             sb.deleteCharAt(sb.length() - 1);
-
+            uri = URI.create(sb.toString());
         }
         else {
             throw new RuntimeException(ERROR_MESSAGE);
         }
 
-        return sb.toString();
+        return uri;
     }
 
     /**
      * OBJECT TO REQUEST PARAMETER
      */
-    public static <T> String makeRequestParam(T t) {
+    public static <T> URI makeRequestParam(T t, String method) {
         if (ObjectUtils.isNotEmpty(t)) {
             ObjectMapper mapper = new ObjectMapper();
-            Map map = mapper.convertValue(t, Map.class);
-            return makeRequestParam(map);
+            Map<String, ?> map = (Map<String, ?>) mapper.convertValue(t, Map.class);
+            return makeRequestParam(map, method);
         }
         else {
             throw new RuntimeException(ERROR_MESSAGE);
