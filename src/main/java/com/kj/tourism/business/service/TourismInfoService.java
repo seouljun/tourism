a551262949c26fc3b271fd85d9f0876c9272adf3
seouljun <kkj5270@naver.com>
2024-05-07 22:24:49 +0900

    리스폰스 받을 수 있게 서비스 수정

--- src/main/java/com/kj/tourism/business/service/TourismInfoService.java
+++ src/main/java/com/kj/tourism/business/service/TourismInfoService.java
@@ -1,10 +1,14 @@
 package com.kj.tourism.business.service;
 
 import com.kj.tourism.base.domain.LocationBased;
+import com.kj.tourism.base.domain.ResponseDomain;
 import com.kj.tourism.base.util.HttpUtil;
+import com.kj.tourism.business.tourismConst.TourismConst;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
@@ -13,9 +17,30 @@ public class TourismInfoService {
     /**
      * 위치기반 관광정보 조회
      */
-    public List<LocationBased> getLocationBasedList(LocationBased locationBasedParam) {
-        HttpUtil.getWebClient("glglglggl");
-        String requestParam = HttpUtil.makeRequestParam(locationBasedParam);
-        return null;
+    public List<ResponseDomain> getLocationBasedList(LocationBased locationBasedParam) {
+        WebClient webClient = HttpUtil.getWebClient(TourismConst.URL);
+        URI requestParam = HttpUtil.makeRequestParam(locationBasedParam, "/locationBasedList1");
+        List<ResponseDomain> block = webClient
+                .get()
+                .uri(requestParam)
+                .retrieve()
+                .bodyToFlux(ResponseDomain.class)
+                .collectList()
+                .block();
+        return block;
     }
+
+//    public static void main(String[] args) {
+//        LocationBased locationBased = new LocationBased();
+//        locationBased.setMobileOS("ETC");
+//        locationBased.setMobileApp("TOUR");
+//        locationBased.set_type("json");
+//        locationBased.setMapX("127");
+//        locationBased.setMapY("37");
+//        locationBased.setRadius(20000L);
+//        locationBased.setServiceKey("d7utwF4c9FJqYh92r4fudpd8ZzR5Yxr%2BUaFW8sRsGpM3XbomM1k6kh7ldUp0d9Lb0F4EUhhdaxm9b%2BVoGii6VQ%3D%3D");
+//
+//        List<ResponseDomain> locationBasedList = getLocationBasedList(locationBased);
+//        log.info(locationBasedList.toString());
+//    }
 }
--- src/test/java/com/kj/tourism/TourismApplicationTests.java
+++ src/test/java/com/kj/tourism/TourismApplicationTests.java
@@ -1,13 +1,32 @@
 package com.kj.tourism;
 
+import com.kj.tourism.base.domain.LocationBased;
+import com.kj.tourism.base.domain.ResponseDomain;
+import com.kj.tourism.business.service.TourismInfoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class TourismApplicationTests {
+    @Autowired
+    TourismInfoService tourismInfoService;
 
     @Test
-    void contextLoads() {
+    void tset() {
+        LocationBased locationBased = new LocationBased();
+        locationBased.setMobileOS("ETC");
+        locationBased.setMobileApp("TOUR");
+        locationBased.set_type("json");
+        locationBased.setMapX("127");
+        locationBased.setMapY("37");
+        locationBased.setRadius(20000L);
+        locationBased.setServiceKey("d7utwF4c9FJqYh92r4fudpd8ZzR5Yxr%2BUaFW8sRsGpM3XbomM1k6kh7ldUp0d9Lb0F4EUhhdaxm9b%2BVoGii6VQ%3D%3D");
+
+        List<ResponseDomain> locationBasedList = tourismInfoService.getLocationBasedList(locationBased);
+        assert locationBasedList != null;
     }
 
 }
