7e3e5d246fd9ae7bc7878f925a15040701e78bc2
seouljun <kkj5270@naver.com>
2024-05-13 19:38:23 +0900

    메인함수 제거

--- src/main/java/com/kj/tourism/business/service/TourismInfoService.java
+++ src/main/java/com/kj/tourism/business/service/TourismInfoService.java
@@ -29,18 +29,4 @@ public class TourismInfoService {
                 .block();
         return block;
     }
-
-//    public static void main(String[] args) {
-//        LocationBased locationBased = new LocationBased();
-//        locationBased.setMobileOS("ETC");
-//        locationBased.setMobileApp("TOUR");
-//        locationBased.set_type("json");
-//        locationBased.setMapX("127");
-//        locationBased.setMapY("37");
-//        locationBased.setRadius(20000L);
-//        locationBased.setServiceKey("d7utwF4c9FJqYh92r4fudpd8ZzR5Yxr%2BUaFW8sRsGpM3XbomM1k6kh7ldUp0d9Lb0F4EUhhdaxm9b%2BVoGii6VQ%3D%3D");
-//
-//        List<ResponseDomain> locationBasedList = getLocationBasedList(locationBased);
-//        log.info(locationBasedList.toString());
-//    }
 }
